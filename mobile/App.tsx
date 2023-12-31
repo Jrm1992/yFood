506a287799a9c99067a86b5b04751abe7c9c91d4
Jrm1992 <marquesjoseroberto@proton.me>
2023-12-30 22:09:03 -0300

    mobile:feat: react-native-paper install

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 import Home from './src/pages/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home />
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Home />
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
