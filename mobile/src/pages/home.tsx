36c53f446c4816ccecad6af6b75b5ea189cbeb39
Jrm1992 <marquesjoseroberto@proton.me>
2023-12-30 21:18:35 -0300

    mobile:feat: home page

---
+++ mobile/src/pages/home.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import Button from '../components/button'
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button>
+        <Text>Button</Text>
+      </Button>
+    </View>
+  )
+}
