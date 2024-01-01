eaa3afb91bfc2bf5e9f8a81dcaebbde07e68594a
Jrm1992 <marquesjoseroberto@proton.me>
2024-01-01 20:02:30 -0300

    web:feat: header component

---
+++ web/components/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <UCard>
+    <header class="flex justify-between">
+      Logo
+
+    <UAvatar
+      src="https://avatars.githubusercontent.com/u/739984?v=4"
+      alt="Avatar"
+    />
+
+    </header>
+  </UCard>
+</template>
