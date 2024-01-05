40d6e10300060ecd2ca4e5d40ec1a49314f7d8f7
Jrm1992 <marquesjoseroberto@proton.me>
2024-01-05 20:54:43 -0300

    server:feat: Restaurant repository

--- server/src/domain/repositories/restaurant.repository.ts
+++ server/src/domain/repositories/restaurant.repository.ts
@@ -3,6 +3,6 @@ import { Restaurant } from '../models/restaurant.model';
 export interface RestaurantRepository {
   getRestaurantByID(restaurantID: string): Promise<Restaurant | null>;
   createRestaurant(restaurant: Restaurant): Promise<Restaurant>;
-  updateRestaurant(restaurant: Restaurant): Promise<Restaurant>;
+  updateRestaurant({ where, data }: { where: { id: string }; data: Restaurant }): Promise<Restaurant>;
   deleteRestaurant(restaurantID: string): Promise<void>;
 }
--- server/src/infrastructure/database/repositories/restaurant.repository.ts
+++ server/src/infrastructure/database/repositories/restaurant.repository.ts
@@ -17,8 +17,8 @@ export class RestaurantRepository implements IRestaurantRepository {
   createRestaurant(restaurant: Restaurant): Promise<Restaurant> {
     return this.prismaService.restaurant.create({ data: restaurant })
   }
-  updateRestaurant(restaurant: Restaurant): Promise<Restaurant> {
-    return this.prismaService.restaurant.update({ data: restaurant, where: { id: restaurant.id } })
+  updateRestaurant({ where, data }: { where: { id: string }; data: Restaurant }): Promise<Restaurant> {
+    return this.prismaService.restaurant.update({ where, data })
   }
   deleteRestaurant(restaurantID: string): Promise<void> {
     this.prismaService.restaurant.delete({ where: { id: restaurantID } })
