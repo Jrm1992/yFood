acdd630b7b43e3551ac600f994d283d19cf813fc
Jrm1992 <marquesjoseroberto@proton.me>
2024-01-08 20:29:58 -0300

    server:feat: Restaurant model update

--- server/src/domain/models/restaurant.model.ts
+++ server/src/domain/models/restaurant.model.ts
@@ -1,9 +1,15 @@
+import { MenuItem } from "./menu-item.model";
+import { Order } from "./order.model";
+
 export class Restaurant {
   constructor(
     public id: string, 
     public name: string, 
     public address: string, 
     public phone: string, 
-    public description: string
+    public description: string,
+    public category: string,
+    public menuItems: MenuItem[],
+    public orders: Order[]
     ) {}
 }
